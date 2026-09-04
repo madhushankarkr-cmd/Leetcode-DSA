9ee6c26d906fcd0bb79d7ec82f060698775ebf48
MADHU SHANKAR KUMAR <madhushankarkr@gmail.com>
2026-09-04 23:17:12 +0530

    [509] [Time Beats: 42.62%] [Memory Beats: 85.60%] - LeetPush

---
+++ 509-Fibonacci-Number.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public int fibo(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public int fib(int n) {
+        return fibo(n);
+    }
+}

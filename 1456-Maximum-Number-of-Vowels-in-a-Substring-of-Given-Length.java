a6223c649ba7d66db7a3bb3ac05da24444369ed6
MADHU SHANKAR KUMAR <madhushankarkr@gmail.com>
2026-09-09 13:26:30 +0530

    [1456] [Time Beats: 81.06%] [Memory Beats: 65.63%] - LeetPush

---
+++ 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.java
@@ -0,0 +1,29 @@
+class Solution {
+    static boolean vowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    public int maxVowels(String s, int k) {
+        int max = 0;
+        int count =0;
+
+        for(int i = 0;i<k;i++){
+            if(vowel(s.charAt(i))){
+                count++;
+            }
+        }
+        max = count;
+
+        for(int i = k;i<s.length();i++){
+            if(vowel(s.charAt(i))){
+                count++;
+            }
+            if(vowel(s.charAt(i - k))){
+                count--;
+            }
+
+            max = Math.max(max,count);
+        }
+        return max;
+       
+    }
+}

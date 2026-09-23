65372f384f3214c2e35f3ebf9779860331711e73
MADHU SHANKAR KUMAR <madhushankarkr@gmail.com>
2026-09-23 22:50:31 +0530

    [724] [Time Beats: 96.23%] [Memory Beats: 22.53%] - LeetPush

--- 724-Find-Pivot-Index.java
+++ 724-Find-Pivot-Index.java
@@ -1,26 +1,29 @@
 class Solution {
-    static int totalsum(int[] nums){
+    static int Totalsum(int[] nums){
         int ts = 0;
-        for(int num: nums){
-            ts = ts + num;
+        int n = nums.length;
+        for(int i =0;i<n;i++){
+            ts +=nums[i];
         }
         return ts;
     }
     static int pivotsum(int[] nums){
-        int sum = totalsum(nums);
         int ls = 0;
-        int n = nums.length;
+        int sum = Totalsum(nums);
 
+        int n = nums.length;
         for(int i =0;i<n;i++){
             int rs = sum - ls - nums[i];
             if(ls == rs){
                 return i;
             }
-            ls = ls += nums[i];
+            ls += nums[i];
         }
         return -1;
     }
+    
     public int pivotIndex(int[] nums) {
         return pivotsum(nums);
+
     }
 }

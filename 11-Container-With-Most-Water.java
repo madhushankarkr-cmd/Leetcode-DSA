443aa278ca85fc15835988ed8789273f7a835323
MADHU SHANKAR KUMAR <madhushankarkr@gmail.com>
2026-09-26 20:20:21 +0530

    [11] [Time Beats: 82.72%] [Memory Beats: 29.60%] - LeetPush

---
+++ 11-Container-With-Most-Water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int max = 0;
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            int width = right - left;
+            int length = Math.min(height[left],height[right]);
+            int area = width * length;
+            max = Math.max(max,area);
+
+            if(height[left]<height[right]){
+                left++;
+            } else {
+                right--;
+            } 
+        }
+       
+        return max;
+        
+    }
+}
